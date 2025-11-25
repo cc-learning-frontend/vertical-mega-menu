bf3a3c64e882704e3a8b27d60daf30c406bf3d1e
Cosmin Condur <cosmincondur@outlook.com>
2025-11-25 11:00:27 +0200

    added functionality

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 "use strict";
 
 // Initial dropdown menu (down)
-const items = document.querySelectorAll('.first-menu-item');
-items.forEach(
+const firstMenuItems = document.querySelectorAll('.first-menu-item');
+const secondMenus = document.querySelectorAll('.second-menu');
+firstMenuItems.forEach(
     item => {
         item.addEventListener('click', () => {
+            secondMenus.forEach(secondMenu => secondMenu.style.display = 'none');
             item.querySelector('.second-menu').style.display = 'block';
         })
     }
@@ -25,10 +27,24 @@ subMenuItems.forEach(
 )
 // Open overlay
 const firstMenuItem = document.querySelectorAll('.first-menu-item');
-firstMenuItem.forEach(item => item.addEventListener('click', () => overlay.style.display = 'block'))
+firstMenuItem.forEach(
+    item => item.addEventListener('click', () => overlay.style.display = 'block')
+)
 // Close overlay
 const secondMenu = document.querySelectorAll('.second-menu');
 overlay.addEventListener('click', function () {
     overlay.style.display = 'none';
     secondMenu.forEach(secondMenu => secondMenu.style.display = 'none');
+    firstMenuItemName.forEach(item => item.classList.remove('is-open'));
 });
+
+// Change color when menu button is clicked
+const firstMenuItemName = document.querySelectorAll('.first-menu-item-name');
+firstMenuItemName.forEach(
+    item => item.addEventListener('click', function () {
+        firstMenuItemName.forEach(i => {
+            if (i !== item) i.classList.remove('is-open');
+        });
+        item.classList.toggle('is-open');
+    })
+)
